e24985390c324ba573245eb11bc45c84c818a8a5
Barak Schoster Goihman <barak@bridgecrew.io>
2020-09-08 23:00:50 +0300

    Create azure.tf (#5)

---
+++ azure.tf
@@ -0,0 +1,11 @@
+resource "azurerm_managed_disk" "example" {
+  name                 = "terragoat-disk-${var.environment}"
+  location             = var.location
+  resource_group_name  = azurerm_resource_group.example.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 1
+  encryption_settings {
+    enabled = false
+  }
+}
